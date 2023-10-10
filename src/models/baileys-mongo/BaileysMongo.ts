d5270b4b9e2507055ab1691c7182562732563083
Matias Rodriguez <matias.rodriguez@cliengo.com>
2023-10-10 09:41:47 -0300

    fix(Error): throw error

--- src/models/baileys-mongo/BaileysMongo.ts
+++ src/models/baileys-mongo/BaileysMongo.ts
@@ -29,7 +29,7 @@ export class BaileysMongo {
         mongoDB: prismaClient
       }
     } catch (err) {
-      throw new Error('Error creating auth in mongo ---- Error')
+      throw new Error(err)
     }
   }
 
